a14d85f66887fdc4ef34866f17963d3b7b742658
BaiDingApp <zyhmkt@163.com>
2019-08-03 21:32:41 +0800

    Initial set up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# yinghui-zhou.github.io
-Personal Homepage
+# 周影辉 的个人主页
+
+### 参考网站 
+在本网站制作过程中参考了下面的网站.
+- https://github.com/VincentGarreau/particles.js
+- https://nrandecker.github.io/particle/
+- http://mikezhang.com/
---
+++ js/app.js
@@ -0,0 +1,119 @@
+/* -----------------------------------------------
+/* Author : Vincent Garreau  - vincentgarreau.com
+/* MIT license: http://opensource.org/licenses/MIT 
+/* GitHub : github.com/VincentGarreau/particles.js 
+/* v2.0.0
+/* ----------------------------------------------- */
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 60,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "random"
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 6
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.75,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 3,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 3,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#fff",
+        "opacity": 1,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 4,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 200,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 200,
+          "size": 20,
+          "duration": 1,
+          "opacity": 1.0,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+  }
+
+);
